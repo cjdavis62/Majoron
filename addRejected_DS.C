77c0807a47b4cd74598d013f994e57415d608e40
cjdavis62 <christopher.davis@yale.edu>
2018-09-17 15:25:10 -0400

    Added new files to reduce a dataset and to add rejected channels (and assign dataset based on livetime)

---
+++ addRejected_DS.C
@@ -0,0 +1,153 @@
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include "TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TLeaf.h"
+#include "TString.h"
+
+using namespace std;
+
+
+bool findChannelInVector(vector<int> &array, int channel)
+{
+  bool flag = false;
+  for (vector<int>::iterator ptr = array.begin(); ptr < array.end(); ptr++) {
+    if (*ptr == channel) {flag = true; break;}
+  }
+  return flag;
+}
+
+void addRejected() {
+    ifstream listFile("ListMC.txt");
+    stringstream ss;
+    string line;
+    string name;
+
+    vector<TString> names;
+
+    while(getline(listFile, line)) {
+        ss.str(line);
+        ss >> name;
+        names.push_back(name.c_str());
+        ss.str("");
+        ss.clear();
+    }
+    listFile.close();
+
+    double lt3021 = 0.0694474153;
+    double lt3018 = 0.0572298326;
+
+    double totLT = lt3021 + lt3018;
+
+    int channel;
+    short int ch;
+
+    map<int, vector<int> > rejectedChannels;
+    ifstream dsFile("DS3021_rejected_channel.txt");
+    while(getline(dsFile, line)) {
+        ss.str(line);
+        ss >> channel;
+
+        rejectedChannels[3021].push_back(channel);
+
+        ss.str("");
+        ss.clear();
+    }
+    dsFile.close();
+    dsFile.open("DS3518_rejected_channel.txt");
+    while(getline(dsFile, line)) {
+        ss.str(line);
+        ss >> channel;
+
+        rejectedChannels[3018].push_back(channel);
+
+        ss.str("");
+        ss.clear();
+    }
+    dsFile.close();
+
+    TString path = "/projects/cuore/simulation/Majoron/v2/"; 
+
+    bool included;
+    int dataset;
+    bool III;
+
+    for(int i = 0; i < names.size(); i++) {
+        TString fileName = path + names[i];
+        cout << fileName << endl;
+
+        TFile* mcFile = new TFile(fileName, "update");
+        TTree* mcTree = (TTree*) mcFile->Get("outTree");
+        mcTree->SetBranchStatus("*", 0);
+        mcTree->SetBranchStatus("Detector", 1);
+
+        TTree* friendTree;
+        if( (friendTree = (TTree*) mcFile->Get("exclChTree")) != NULL ) {
+            mcTree->RemoveFriend(friendTree);
+            friendTree->Delete("all");
+        }
+        friendTree = new TTree("exclChTree", "");
+
+        III = false;
+
+        TString type = mcTree->GetBranch("Detector")->GetLeaf("Detector")->GetTypeName();
+        if( type == "Int_t" ) {
+            mcTree->SetBranchAddress("Detector", &channel);
+            III = true;
+        } else {
+            mcTree->SetBranchAddress("Detector", &ch);
+        }
+
+        friendTree->Branch("Dataset", &dataset, "Dataset/I");
+        friendTree->Branch("Included", &included, "Included/O");
+
+        for(Long64_t e = 0; e < mcTree->GetEntries(); e++) {
+            mcTree->GetEntry(e);
+
+	    percent = std::floor(((double)i * 100.0) / mcTree->GetEntries());
+	    if ( percent > percent_previous)
+	      {
+		std::cout << "Entries read: " << percent << "%" << "\r" << std::flush;
+		percent_previous = percent;
+	      }
+	    
+            double rnd = rand() * totLT / (double)RAND_MAX;
+            if( rnd < lt3021 )
+                dataset = 3021;
+            else
+                dataset = 3018;
+
+            included = true;
+            
+            if( III ) {
+	      if (findChannelInVector(rejectedChannels[dataset], channel)) {
+	      //if( std::find(rejectedChannels[dataset].begin(), rejectedChannels[dataset].end(), channel) != rejectedChannels[dataset].end()) {
+                    included = false;
+		    
+                }
+            } else {
+	      if (findChannelInVector(rejectedChannels[dataset], ch)) {
+	      //if( std::find(rejectedChannels[dataset].begin(), rejectedChannels[dataset].end(), ch) != rejectedChannels[dataset].end()) {
+                    included = false;
+                }
+            }
+
+            friendTree->Fill();
+        }
+        mcTree->SetBranchStatus("*", 1);
+        mcTree->AddFriend(friendTree);
+        mcTree->Write("", TObject::kOverwrite);
+        friendTree->Write();
+        mcFile->Close();
+        delete mcFile;
+    }
+}
+
+int main() {
+    addRejected();
+}
---
+++ frac_reduce.C
@@ -0,0 +1,94 @@
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TMath.h"
+#include "TString.h"
+#include "TCut.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TLine.h"
+#include "TFile.h"
+#include "TF1.h"
+#include "TH1F.h"
+#include <math.h>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <stdexcept>
+#include "TComplex"
+
+void frac_reduce() {
+
+  TFile * inpFile = new TFile("/projects/cuore/data/ds3018_3021/Reduced_bkg_3018_3021.root");
+  TFile * outFile = new TFile("/projects/cuore/data/ds3018_3021/Reduced_bkg_3018_3021_10percent.root", "Recreate");
+    
+  TTree * inpTree = (TTree*)inpFile->Get("outTree");
+
+  TTree * outTree = new TTree("outTree", "fractional reduced file");
+  
+  int Channel, Detector;
+  int Multiplicity;
+  int Run;
+  double Energy;
+  double TotalEnergy;
+  double Time;
+  int MultipletIndex;
+  bool PSA;
+  int Layer;
+  int Tower;
+  int Floor;
+
+  int test_val = 0;
+
+
+  inpTree->SetBranchAddress("Channel", &Channel);
+  inpTree->SetBranchAddress("Multiplicity", &Multiplicity);
+  inpTree->SetBranchAddress("Run", &Run);
+  inpTree->SetBranchAddress("Energy", &Energy);
+  inpTree->SetBranchAddress("TotalEnergy", &TotalEnergy);
+  inpTree->SetBranchAddress("Time", &Time);
+  inpTree->SetBranchAddress("MultipletIndex", &MultipletIndex);
+  inpTree->SetBranchAddress("PSA", &PSA);
+  inpTree->SetBranchAddress("Layer", &Layer);
+  inpTree->SetBranchAddress("Tower", &Tower);
+  inpTree->SetBranchAddress("Floor", &Floor);
+
+
+  outTree->Branch("Channel", &Channel, "Channel/I");
+  outTree->Branch("Detector", &Detector, "Detector/I");
+  outTree->Branch("Multiplicity", &Multiplicity, "Multiplicity/I");
+  outTree->Branch("Run", &Run, "Run/I");
+  outTree->Branch("Energy", &Energy, "Energy/D");
+  outTree->Branch("TotalEnergy", &TotalEnergy, "TotalEnergy/D");
+  outTree->Branch("Time", &Time, "Time/D");
+  outTree->Branch("MultipletIndex", &MultipletIndex, "MultipletIndex/I");
+  outTree->Branch("PSA", &PSA, "PSA/B");
+  outTree->Branch("Layer", &Layer, "Layer/I");
+  outTree->Branch("Tower", &Tower, "Tower/I");
+  outTree->Branch("Floor", &Floor, "Floor/I");
+
+  outTree->SetAlias("Ener2", "Energy");
+
+  int seed = 1000;
+
+  TRandom3 * random = new TRandom3(seed);
+
+  for (int i = 0; i < inpTree->GetEntries(); i++) {
+    inpTree->GetEntry(i);
+
+    int j = random->Integer(10);
+    if (j == 0) // take 10 percent of the values
+    {
+      Detector = Channel;
+      //std::cout << j << std::endl;
+      outTree->Fill();
+      // write the outTree
+    }
+    
+  }
+
+    outTree->Write();
+    outFile->Close();
+}
