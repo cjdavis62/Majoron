e6dbc82620468f4edec786cb3cb9749729073781
cjdavis62 <christopher.davis@yale.edu>
2019-01-04 18:05:41 -0500

    Now runs over all MC and can iterate to any arbitrary number of pseudoexperiments. Need to check on the actual randomness of the generator between sets. Don't want to have multiple iterations with the same statistics.

--- sensitivity/GenerateToyMC.py
+++ sensitivity/GenerateToyMC.py
@@ -16,34 +16,49 @@ def add_MC_dir(MC_file):
 def Read_MC_files(filename):
     # give the datatype for the elements
     dtype_MC_files = np.dtype([('filename', np.unicode_,64), ('norm_factor', np.float64)])
-    MC_file = np.loadtxt(filename, dtype = dtype_MC_files)
+    MC_file = np.loadtxt(filename, dtype = dtype_MC_files, skiprows = 1)
 
     return MC_file
 
 # Use sed on the template script to change the files
-def generate_from_template(template_filename, normalization_factor, livetime_factor):
+def generate_from_template(template_filename, filename, normalization_factor, livetime_factor):
 
     temp_name_A = "GeneratorScripts/" + template_filename + "_tempA"
     temp_name_B = "GeneratorScripts/" + template_filename + "_tempB"
     os.system("sed 's/__normalization_factor__/%s/' < GenerateToyMC_template.cxx  > %s" %(normalization_factor, temp_name_A))
     os.system("sed 's/__livetime_factor__/%s/' < %s > %s" %(livetime_factor, temp_name_A, temp_name_B))
-    os.system("mv %s GeneratorScripts/%s; rm %s" %(temp_name_B, template_filename, temp_name_A))
+    os.system("sed 's/__filename__/%s/' < %s > %s" %(filename, temp_name_B, temp_name_A))
+    os.system("mv %s GeneratorScripts/%s.cxx; rm %s" %(temp_name_A, template_filename, temp_name_B))
             
 # loop over all the needed MC files
 def Loop_over_MC_files(read_file_array, livetime_factor):
     for file_name, normalization in read_file_array:
         # Create template filename
         template_file_name = "GenerateToyMC_" + file_name[:-5]
-        generate_from_template(template_file_name, normalization, livetime_factor)
+        generate_from_template(template_file_name, file_name[:-5], normalization, livetime_factor)
 
+        # move the file over so it can be run
+        os.system("cp GeneratorScripts/%s.cxx GeneratorScripts/GenerateToyMC.cpp" %(template_file_name))
+        os.system("root -b -q GeneratorScripts/GenerateToyMC.cpp")
+        os.system("rm GeneratorScripts/GenerateToyMC.cpp")
+
+# Add up all the created MC
+def hadd_MC(iteration):
+    os.system("hadd -f /projects/cuore/simulation/Majoron/sensitivity/MC_iterations/MC_data_%s.root /projects/cuore/simulation/Majoron/sensitivity/*.root" %(iteration))
+
+def clean_outputs():
+    os.system("rm /projects/cuore/simulation/Majoron/sensitivity/*.root")
+    
 # Generate N ToyMC
-def Generate_N_sets():
-    do_stuff()
+def Generate_N_sets(number_of_iterations, read_file_array, livetime_factor):
+    for iteration in range(number_of_iterations):
+        Loop_over_MC_files(read_file_array, livetime_factor)
+        hadd_MC(iteration)
+        clean_outputs()
 
 # main stuffs
 livetime_factor = 1
 read_file_array = Read_MC_files("FileList.txt")
 
-Loop_over_MC_files(read_file_array, livetime_factor)
+Generate_N_sets(20, read_file_array, livetime_factor)
 
-#edit_template("GenerateToyMC_template.cxx", )
--- sensitivity/GenerateToyMC_template.cxx
+++ sensitivity/GenerateToyMC_template.cxx
@@ -1,15 +1,105 @@
-{
+#include <TRandom3.h>
+#include "TMath.h"
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include "TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TLeaf.h"
+#include "TString.h"
 
+
+void GenerateToyMC(){
+  
   // Get the file to generate from
-  //TFile *MC_file = new TFile("_filename_");
-
+  TFile *MC_file = new TFile("/projects/cuore/simulation/BkgSimulations/ntp/__filename__.root");
+  // Get the file to be created
+  TFile *out_file = new TFile("/projects/cuore/simulation/Majoron/sensitivity/__filename__.root", "RECREATE");
+  
+  TTree *MC_tree = (TTree*)MC_file->Get("outTree");
+  TTree *MC_friend_tree = (TTree*)MC_file->Get("exclChTree");
   // The normalization factor
   double normalization_factor = __normalization_factor__;
   // The livetime factor
   double livetime_factor = __livetime_factor__;
   // number of events to generate
-  double events = normalization_factor * livetime_factor;
+  double sampled_factor = normalization_factor * livetime_factor;
 
-  std::cout << normalization_factor << std::endl;
+  // Grab events, and round down the number of events to sample
+  int sampled_events = TMath::Floor(MC_tree->GetEntries() * sampled_factor);
+
+  
+  // The output trees
+  TTree * outTree = new TTree("outTree", "");
+  TTree * friendTree = new TTree("exclChTree", "");
+
+  if (sampled_factor > 1)
+    {
+      std::cout << "Sampling more events than in histogram for file: " << MC_file << std::endl;
+      std::cout << "Sampling factor: " << sampled_factor << std::endl;
+    }
+  std::cout << "Sampling " << sampled_events << " events of " << MC_tree->GetEntries() << " from file " << "__filename__" << std::endl;
+
+  // Get the interesting branches
+  int Channel;
+  int Detector;
+  double Ener2;
+  double ESum2;
+  short int Multiplicity;
+  int Layer;
+  int Tower;
+  int Floor;
+  int Dataset;
+  bool Included;
+
+  MC_tree->SetBranchAddress("Channel", &Channel);
+  MC_tree->SetBranchAddress("Detector", &Detector);
+  MC_tree->SetBranchAddress("Ener2", &Ener2);
+  MC_tree->SetBranchAddress("ESum2", &ESum2);
+  MC_tree->SetBranchAddress("Multiplicity", &Multiplicity);
+  MC_tree->SetBranchAddress("Layer", &Layer);
+  MC_tree->SetBranchAddress("Tower", &Tower);
+  MC_tree->SetBranchAddress("Floor", &Floor);
+
+  MC_friend_tree->SetBranchAddress("Dataset", &Dataset);
+  MC_friend_tree->SetBranchAddress("Included", &Included);
+
+  
+  outTree->Branch("Channel", &Channel, "Channel/I");
+  outTree->Branch("Detector", &Detector, "Detector/I");
+  outTree->Branch("Ener2", &Ener2, "Ener2/D");
+  outTree->Branch("ESum2", &ESum2, "ESum2/D");
+  outTree->Branch("Multiplicity", &Multiplicity, "Multiplicity/S");
+  outTree->Branch("Layer", &Layer, "Layer/I");
+  outTree->Branch("Tower", &Tower, "Tower/I");
+  outTree->Branch("Floor", &Floor, "Floor/I");
+
+  friendTree->Branch("Dataset", &Dataset, "Dataset/I");
+  friendTree->Branch("Included", &Included, "Included/O");
+
+  // Start random number generator
+  TRandom3 *rand = new TRandom3();
+  
+  // Sample events from the histogram
+  for (int i = 0; i < sampled_events; i++) {
+    int event_to_sample = rand->Integer(sampled_events);
+    MC_tree->GetEntry(i);
+    outTree->Fill();
+    MC_friend_tree->GetEntry(i);
+    friendTree->Fill();
+  }
+
+
+
+  MC_file->Close();
+  outTree->AddFriend(friendTree);
+  outTree->Write();
+  friendTree->Write();
+  out_file->Close();
   
 }
