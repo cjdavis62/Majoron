b5fd2d815217f57d76d1db1e71a6d616db1a0830
cjdavis62 <christopher.davis@yale.edu>
2019-04-30 15:59:42 -0400

    reduced the number of arguments.
    Now needs fewer arguments to run the JAGS script. This script is now paired with the job having the setup file already contained.

--- scripts/start_JAGS.py
+++ scripts/start_JAGS.py
@@ -19,23 +19,17 @@ import math
 # read arguments
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--spectral_index", type=int, help="The spectral index, i.e. 1, 2, 3, 7")
     parser.add_argument("-d", "--data_path", type=str, help="The path to the data directory")
     parser.add_argument("-D", "--data_name", type=str, help="The name of the data file. Do not include the '.root'")
     parser.add_argument("-m", "--mc_path", type=str, help="The path the MC directory")
     parser.add_argument("-w", "--work_dir", type=str, help="The path to the working directory")
-    #parser.add_argument("-p", "--setup_path", type=str, help="The path to the setup files, e.g. '/home/cjdavis/JAGS_setup'")
     parser.add_argument("-M", "--multiplicities", type=str, action="append", help="The multiplicities to consider, viz. M1, M2, and/or M2sum. Input as '-M M1 -M M2 -M M2sum'")
-    #parser.add_argument("-s", "--systematic", type=str, help="The name of the systematic, e.g. OddTowers or NoT12")
     args = parser.parse_args()
 
     # check that inputs point to existing places
     if not(os.path.isfile(args.data_path + "/" + args.data_name + ".root")):
         print("Error: data file not found at path: {data_path}/{data_name}.root".format(data_path=args.data_path, data_name=args.data_name))
         sys.exit(1)
-#    if not(os.path.isfile("{setup_path}/n{spectral_index}/setup_{systematic}_n{spectral_index}.sh".format(setup_path=args.setup_path, spectral_index=args.spectral_index, systematic=args.systematic))):
-#        print("Error: setup file not found at path: {setup_path}/n{spectral_index}/setup_{systematic}_n{spectral_index}.sh".format(setup_path=args.setup_path, spectral_index=args.spectral_index, systematic=args.systematic))
- #       sys.exit(1)
     if not(os.path.isdir(args.mc_path)):
         print("Error: MC directory does not exist at path: {mc_path}".format(mc_path=args.mc_path))
         sys.exit(1)
